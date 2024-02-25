39ca28f359134c9bdbe5e6acde0b9a7d5983c136
Ankitaghavate <144261424+Ankitaghavate@users.noreply.github.com>
2024-02-26 01:04:37 +0530

    Update index.html

--- index.php
+++ index.php
@@ -68,19 +68,6 @@
             window.location.href = "http://localhost/Notice/view_notice.php";
         });
             </script>
-        <?php
-        if($_SERVER['REQUEST_METHOD'] == 'POST')
-        {
-        if (isset($_POST['submit'])) {
-            $title = $_POST['title'];
-            $content = $_POST['content'];
-          
-
-            file_put_contents('notices.txt', "$title: $content" . PHP_EOL, FILE_APPEND);
-           // echo '<script>alert("Notice Added Successfully...!!!");</script>';
-        }
-    }
-        ?>
     </div>
 </body>
 </html>
